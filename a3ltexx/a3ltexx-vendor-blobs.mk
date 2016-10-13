b9a04509a475402de00a4115e5f09d68170ad1fa
vlw <vlwwwwww@gmail.com>
2016-10-13 22:03:40 +0300

    Fix build error
    Change-Id: If73dbfa5f45d36a304cec2a7b746491d18eb2984

--- a3ltexx/a3ltexx-vendor-blobs.mk
+++ a3ltexx/a3ltexx-vendor-blobs.mk
@@ -249,7 +249,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a3ltexx/proprietary/vendor/lib/libthermalioctl.so:system/vendor/lib/libthermalioctl.so \
     vendor/samsung/a3ltexx/proprietary/bin/time_daemon:system/bin/time_daemon \
     vendor/samsung/a3ltexx/proprietary/app/TimeService/TimeService.apk:system/app/TimeService/TimeService.apk \
-    vendor/samsung/a3ltexx/proprietary/lib/libtime_genoff.so:system/lib/libtime_genoff.so
+    vendor/samsung/a3ltexx/proprietary/lib/libtime_genoff.so:system/lib/libtime_genoff.so \
     vendor/samsung/a3ltexx/proprietary/lib/libcommon_time_client.so:system/lib/libcommon_time_client.so \
     vendor/samsung/a3ltexx/proprietary/priv-app/FusedLocation/FusedLocation.apk:system/priv-app/FusedLocation/FusedLocation.apk \
     vendor/samsung/a3ltexx/proprietary/bin/ssr_diag:system/bin/ssr_diag \
