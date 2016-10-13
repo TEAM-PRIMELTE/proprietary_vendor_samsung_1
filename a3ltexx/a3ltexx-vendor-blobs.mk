900583659e67f6da0397b777b2f2bfba55dc522e
vlw <vlwwwwww@gmail.com>
2016-10-13 20:00:38 +0300

    Fix GPS
    Change-Id: I2383f6d0a0bc9700ea0a25ac0740c35e18f6bc32

--- a3ltexx/a3ltexx-vendor-blobs.mk
+++ a3ltexx/a3ltexx-vendor-blobs.mk
@@ -221,6 +221,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/a3ltexx/proprietary/app/TimeService/TimeService.apk:system/app/TimeService/TimeService.apk \
     vendor/samsung/a3ltexx/proprietary/lib/libtime_genoff.so:system/lib/libtime_genoff.so
     vendor/samsung/a3ltexx/proprietary/lib/libcommon_time_client.so:system/lib/libcommon_time_client.so \
+    vendor/samsung/a3ltexx/proprietary/priv-app/FusedLocation/FusedLocation.apk:system/priv-app/FusedLocation/FusedLocation.apk \
     vendor/samsung/a3ltexx/proprietary/bin/ssr_diag:system/bin/ssr_diag \
     vendor/samsung/a3ltexx/proprietary/bin/ssr_setup:system/bin/ssr_setup \
     vendor/samsung/a3ltexx/proprietary/bin/subsystem_ramdump:system/bin/subsystem_ramdump \
