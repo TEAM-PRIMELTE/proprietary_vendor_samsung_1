0fe5180f295fec868bdf104640250eeeaae600ab
vlw <vlwwwwww@gmail.com>
2017-01-05 10:11:16 +0300

    Cleanning
    Change-Id: I32feaf4ca7cfe9a264b8a399836feafc9a811e2c

--- a3ltexx/Android.mk
+++ a3ltexx/Android.mk
@@ -19,17 +19,6 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(BOARD_VENDOR),samsung)
 ifneq ($(filter a3ltexx,$(TARGET_DEVICE)),)
 
-#include $(CLEAR_VARS)
-#LOCAL_MODULE := FusedLocationSTK
-#LOCAL_MODULE_OWNER := samsung
-#LOCAL_SRC_FILES := proprietary/priv-app/FusedLocation/FusedLocation.apk
-#LOCAL_MODULE_TAGS := optional
-#LOCAL_MODULE_SUFFIX := $(COMMON_ANDROID_PACKAGE_SUFFIX)
-#LOCAL_MODULE_CLASS := APPS
-#LOCAL_PRIVILEGED_MODULE := true
-#LOCAL_CERTIFICATE := platform
-#include $(BUILD_PREBUILT)
-
 include $(CLEAR_VARS)
 LOCAL_MODULE := libloc_api_v02
 LOCAL_MODULE_OWNER := qualcomm
